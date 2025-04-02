10ea46fdbb4757452421e195ac2672a40408a6c8
RAMANATHAN K <118366497+rama1832nathan@users.noreply.github.com>
2025-04-02 09:08:45 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
         
  
-        <div className="text-2xl font-extrabold text-amber-500 font-lucky text-stroke">
+        <div className="text-4xl font-extrabold text-amber-500 font-lucky text-stroke">
           Ramanathan   K
         </div>
 
